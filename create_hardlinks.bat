1708ab621ee6951d8cd475c10d331dcdcdac2bc4
Turukano <kennel@posteo.de>
2023-06-17 21:17:49 +0200

    Anonymize bat

--- create_hardlinks.bat
+++ create_hardlinks.bat
@@ -1,7 +1,8 @@
 @set /p input=Enter keyboardmanufacturer\keyboardname\keymaps\yourkeymapfolder which you would like to build :
-fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\combos.h %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\turukano\combos.h
-fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\definitions.h %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\turukano\definitions.h
-fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\keymap.c %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\turukano\keymap.c
-fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\macros.h %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\turukano\macros.h
-fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\tapdances.h %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\turukano\tapdances.h
-fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\rules.mk %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\turukano\rules.mk
+@set /p uspace=Enter the name of your userspace-folder from which you will copy these files :
+fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\combos.h %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\%uspace%\combos.h
+fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\definitions.h %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\%uspace%\definitions.h
+fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\keymap.c %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\%uspace%\keymap.c
+fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\macros.h %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\%uspace%\macros.h
+fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\tapdances.h %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\%uspace%\tapdances.h
+fsutil hardlink create %HOMEDRIVE%%HOMEPATH%\qmk_firmware\keyboards\%input%\rules.mk %HOMEDRIVE%%HOMEPATH%\qmk_firmware\users\%uspace%\rules.mk
